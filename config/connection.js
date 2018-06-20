549bce61efec14a9ce9ef84b5a3355ba010ddce3
Nicole Gould <nicolegould@Nicoles-MacBook-Air.local>
2018-06-19 23:13:10 -0400

    add

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,11 @@
 var mongoose = require("mongoose");
 
-
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
  //Local Database Configuration with Mongoose
-
- mongoose.connect("mongodb://localhost/allthenewsdb", function(error)
+ mongoose.Promise = Promise;
+ mongoose.connect(MONGODB_URI, function(error)
 
  	{if(error) throw error;
 
